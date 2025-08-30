c882270d7f1a9bc03d63e07d1c9f99b83c65a553
JAINE LIN <jainelinje@gmail.com>
2025-08-31 02:25:21 +0800

    added basic dashboard metrics

--- web-app/src/dashboard.jsx
+++ web-app/src/dashboard.jsx
@@ -8,29 +8,48 @@ export default function Dashboard() {
         <h1>Analytics Dashboard</h1>
         <p>Comprehensive insights into review quality, policy violations, and system performance across all analysed locations</p>
         </div>
+        
+        <div style={{display: "flex", flexDirection: "row", gap: "20px"}}>
 
+        <div className="pageContainer">
+        <h4 className="graphs">Total Reviews</h4>
+        <p className="graphs">3,438</p>
+        </div>
+
+        <div className="pageContainer">
+        <h4 className="graphs">Top location category</h4>
+        <p className="graphs">Restaurant</p>
+        </div>
+
+        <div className="pageContainer">
+        <h4 className="graphs">"relevant and quality" reviews</h4>
+        <p className="graphs">966</p>
+        </div>
+
+        </div>
+        
         <div className="pageContainer">
         <h4 className="graphs">Model Performance</h4>
         <p className="graphs">overall</p>
         <div style={{display: "flex", flexDirection: "row"}}>
         
         <div className="pageContainer">
-        <p>accuracy</p>
+        <p className="graphs">accuracy</p>
         <p style={{color: "#ff746c", fontWeight: "bold"}}>84%</p>
         </div>
         
         <div className="pageContainer">
-        <p>precision</p>
+        <p className="graphs">precision</p>
         <p style={{color: "#8884d8", fontWeight: "bold"}}>84%</p>
         </div>
         
         <div className="pageContainer">
-        <p>recall</p>
+        <p className="graphs">recall</p>
         <p style={{color: "#82ca9d", fontWeight: "bold"}}>84%</p>
         </div>
         
         <div className="pageContainer">
-        <p>f1</p>
+        <p className="graphs">f1</p>
         <p style={{color: "#ffc658", fontWeight: "bold"}}>83%</p>
         </div>
         
