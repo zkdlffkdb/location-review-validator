7b244930ce5c3bd9c1a0fb7f4930da1877c4daac
JAINE LIN <jainelinje@gmail.com>
2025-08-31 02:07:34 +0800

    added overall model performance analysis

--- web-app/src/dashboard.jsx
+++ web-app/src/dashboard.jsx
@@ -9,8 +9,32 @@ export default function Dashboard() {
         <p>Comprehensive insights into review quality, policy violations, and system performance across all analysed locations</p>
         </div>
 
-        <div style={{display: "flex", flexDirection: "row", gap: "20px"}}>
-        <div className="pageContainer" />
+        <div className="pageContainer">
+        <h4 className="graphs">Model Performance</h4>
+        <p className="graphs">overall</p>
+        <div style={{display: "flex", flexDirection: "row"}}>
+        
+        <div className="pageContainer">
+        <p>accuracy</p>
+        <p style={{color: "#ff746c", fontWeight: "bold"}}>84%</p>
+        </div>
+        
+        <div className="pageContainer">
+        <p>precision</p>
+        <p style={{color: "#8884d8", fontWeight: "bold"}}>84%</p>
+        </div>
+        
+        <div className="pageContainer">
+        <p>recall</p>
+        <p style={{color: "#82ca9d", fontWeight: "bold"}}>84%</p>
+        </div>
+        
+        <div className="pageContainer">
+        <p>f1</p>
+        <p style={{color: "#ffc658", fontWeight: "bold"}}>83%</p>
+        </div>
+        
+        </div>
         </div>
             <div className="pageContainer">
             <h4 className="graphs">Model Performance</h4>
